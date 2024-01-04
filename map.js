bca901198f8ecfc9b901154a3fdd7d3f470376d3
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-04 23:45:18 +0530

    mape

---
+++ map.js
@@ -0,0 +1,7 @@
+const myNum=[1,2,3,4,5,6,7,8,9,10];
+
+//const newnum=myNum.map((num)=>num+10);
+
+const newnum=myNum.map((num)=>num*10).map((num)=>num+1)
+.filter((num)=> num>=40);
+console.log(newnum)
