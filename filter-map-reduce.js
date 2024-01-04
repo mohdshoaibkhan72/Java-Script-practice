d9a918ba1b70ae3dbeb327ef82099cacc2704808
mohammad shoaib khan <mohdshoaibkhan7211@gmail.com>
2024-01-04 22:59:29 +0530

    first commit

---
+++ filter-map-reduce.js
@@ -0,0 +1,19 @@
+// const coding=["js","ruby","java","python","cpp"];
+
+// coding.forEach((item)=>{
+//     console.log(item)
+// })
+
+// const myNum=[1,2,3,4,5,6,7,8,9,10];
+//  //const  newnum=myNum.filter( (num)=>num>5);//wothout retuen ist work 
+
+// //if the scop in function you must have white the retun value
+//  const newnum=myNum.filter((num)=>{
+    
+    
+//     return num>4;
+//  })
+//  console.log(newnum);
+
+
+  
